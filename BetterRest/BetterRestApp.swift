018c69a0c34558a4da2e08d3c51864cdc844db53
nomad483 <nicolaizakluka@gmail.com>
2023-04-16 16:31:03 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mykola Zakluka on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
